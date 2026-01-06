70d595314e4fbd9e74e81b85f45cbdf416ff12d9
Makun <mathieubrt1609@gmail.com>
2026-01-07 00:24:14 +0100

    fresh project

---
+++ Projet_Annuel_Village_Defender/Assets/ECS/Authoring/EnemyAuthoring.cs
@@ -0,0 +1,25 @@
+﻿using ECS.Components;
+using UnityEngine;
+using Unity.Entities;
+
+namespace ECS.Authoring
+{
+    public class EnemyAuthoring : MonoBehaviour
+    {
+        public float speed = 3f;
+
+        class Baker : Baker<EnemyAuthoring>
+        {
+            public override void Bake(EnemyAuthoring authoring)
+            {
+                Entity entity = GetEntity(TransformUsageFlags.Dynamic);
+                
+                AddComponent(entity, new MoveSpeed
+                {
+                    Value = authoring.speed
+                });
+                AddComponent<EnemyTag>(entity);
+            }
+        }
+    }
+}
