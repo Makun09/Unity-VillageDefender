ab79339e9c66e02946d07003852167fd9835f32e
Suussonic <150890672+Suussonic@users.noreply.github.com>
2026-01-11 20:06:21 +0100

    Tour Spawnable

---
+++ Projet_Annuel_Village_Defender/Assets/Scripts/SpawnTower.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace LP.SpawnTower
+{
+    public class SpawnTower : MonoBehaviour
+    {
+        [SerializeField] GameObject towerPrefab = null;
+
+        private Camera cam = null;
+
+        private void Start()
+        {
+            cam = Camera.main;
+        }
+
+        private void Update()
+        {
+            SpawnAtMousePos();
+        }
+
+        private void SpawnAtMousePos()
+        {
+            if (Mouse.current.leftButton.wasPressedThisFrame)
+            {
+                Ray ray = cam.ScreenPointToRay(Mouse.current.position.ReadValue());
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    Instantiate(towerPrefab, hit.point, Quaternion.identity);
+                }
+            }
+        }
+        
+    }
+}
+
