97dd5478ef2550bbef9795a2ba7db14fc09c45aa
Makun <mathieubrt1609@gmail.com>
2026-03-23 19:12:03 +0100

    add goblin behavior and spawning systems with rise and walk mechanics

---
+++ Projet_Annuel_Village_Defender/Assets/Scripts/ECS/Authoring/Enemy/Goblin/GoblinTargetAuthoring.cs
@@ -0,0 +1,19 @@
+﻿using ECS.Authorings.Enemy.Goblin;
+using Unity.Entities;
+using UnityEngine;
+
+namespace ECS.Authorings.Enemy.Goblin
+{
+    public class GoblinTargetAuthoring : MonoBehaviour
+    {
+        private void Start()
+        {
+            var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+            var entity = entityManager.CreateEntity();
+            entityManager.AddComponentData(entity, new GoblinTargetPosition
+            {
+                Value = transform.position
+            });
+        }
+    }
+}
---
+++ Projet_Annuel_Village_Defender/Assets/Scripts/ECS/Components/Enemy/AgressiveBalloon/GoblinWalkProperties.cs
@@ -0,0 +1,15 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+namespace ECS.Components.Goblin
+{
+    public struct GoblinWalkProperties : IComponentData , IEnableableComponent
+    {
+        public float WalkSpeed;
+    }
+    
+    public struct GoblinHeading : IComponentData , IEnableableComponent
+    {
+        public float3 Value;
+    }
+}
